e240661240f65e8c647aa617202d85dc35204701
saranascimento <sarah.gomes80@hotmail.com>
2021-08-19 12:34:28 -0300

    Adiciona tipagem aos componentes

--- 01-github-explorer/usando-typescript/src/components/RepositoryList.tsx
+++ 01-github-explorer/usando-typescript/src/components/RepositoryList.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { RepositoryItem } from './RepositoryItem';
 import '../styles/repositories.scss';
 
+interface Repository {
+  name: string,
+  description: string,
+  html_url: string,
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = React.useState([]);
+  const [repositories, setRepositories] = React.useState<Repository[]>([]);
 
   React.useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
